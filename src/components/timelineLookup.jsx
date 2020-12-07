a54269852a97230bc962cc270fb64c15b859fffd
Dan Wahlin <danwahlin@iMac-Office.local>
2020-12-07 14:20:13 -0700

    General cleanup

--- src/components/timelineLookup.jsx
+++ src/components/timelineLookup.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 import { TimeLine } from "./timeline";
 
 export const TimeLineLookup = () => {
@@ -7,17 +6,17 @@ export const TimeLineLookup = () => {
     const stepsData = [
         {label: 'Refund requested', subLabel:"3rd May 14:04", type: 'success', description: 'We have received your request and will assign it to one of our agents.'},
         {label: 'In progress', subLabel:"4th May 10:25", type: 'success', description: 'We’ll review your request and apply for any available refund from the carrier(s).'},
-        {label: 'Carrier approved', subLabel:' ', type: '', description: 'Carrier approved refund.'},
+        {label: 'Carrier approved', subLabel:' ', type: 'warning', description: 'Carrier approved refund.'},
         {label: 'Refund completed', subLabel:' ', type: '', description: 'Refund issued.'}
     ];
 
     const lookupTimeline = () => {
-        setSteps(stepsData);
+        setSteps(stepsData); 
     }
-
+    
     return (
         <div>
-            <input type="text" placeholder="Order Number" /> 
+            <input type="text" placeholder="Order Number" defaultValue={''} /> 
             <button onClick={lookupTimeline}>View Timeline</button>
             <br /><br />
             <TimeLine steps={steps}></TimeLine>
